7f665db6e6a9cd77adfd2500aa02462cf5c7f1a8
Stepan Lopushansky <stepan.te.ua@gmail.com>
2015-09-03 01:18:36 +0300

    update project version, fix styles, add new data

--- server/projectsData.js
+++ server/projectsData.js
@@ -1,7 +1,18 @@
 if (Projects.find().count() === 0) {
     Projects.insert({
-        title: 'My own website',
+        title: 'Personal website',
         description: 'I created this project to show You my skills.',
-        url: 'http://localhost:3000'
+        url: 'http://ahio.meteor.com'
+    });
+
+    Projects.insert ({
+       title: 'Venues search',
+       description: "This project is a Single Page Application using Meteor, where I made " +
+        "authorization, integration with Foursquare and Google maps APIs. " +
+        "After authorization, users can enter keyword of places and get info " +
+        "about all places that match the keyword in some radius. Also, user " +
+        "can save this info to CSV file.",
+       url: 'http://meteor-gmaps.meteor.com/'
     })
+
 }
